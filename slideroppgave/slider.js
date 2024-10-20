c7a4944c460abba71238eb22a5a7c0112a007b73
William B <william.buwembo21@gmail.com>
2024-10-20 13:11:15 +0200

    added imgades and slider

--- slideroppgave/slider.js
+++ slideroppgave/slider.js
@@ -1,8 +1,30 @@
 console.log("it works");
 
-const app = document.getElementById("app");
+const left = document.querySelector(".left");
+const right = document.querySelector(".right");
+const slider = document.querySelector(".slider");
+const images = document.querySelectorAll(".image");
 
-updateView();
-function updateView() {
-  app.innerHTML = ``;
-}
+let slideNumber = 1;
+const length = images.length;
+
+const nextSlide = () => {
+    slider.style.transform = `translateX(-${slideNumber * 800}px)`;
+    slideNumber++;
+};
+
+const getFirstSlide = () => {
+    slider.style.transform = `translateX(-0px)`;
+    slideNumber = 1;
+};
+
+right.addEventListener("click", () => {
+    slideNumber < length ? nextSlide() : getFirstSlide();
+    console.log(slideNumber);
+});
+
+//const app = document.getElementById("app");
+//updateView();
+//function updateView() {
+//    app.innerHTML = ``;
+//}
