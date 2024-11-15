a763e285ea9dada2236d2d120f5c4d315dc4f3b5
William B <william.buwembo21@gmail.com>
2024-11-15 14:40:48 +0100

    emne 3

---
+++ emne-3/Uke1-2/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // oppgave 1 lag 8 datatyper
+            int num1 = 10;
+            float num2 = 1.0f;
+            double num3 = 1.0;
+            decimal num4 = 3.3m;
+            bool ifTrue = true;
+            int[] arr = { 1, 2, 3};
+            string text1 = "Hello, World!";
+            char text2 = 'B';
+
+            Console.WriteLine(text1);
+            Console.ReadLine();
+        }
+    }
+}
