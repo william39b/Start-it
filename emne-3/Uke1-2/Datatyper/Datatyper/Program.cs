5b8bb86f70cb2dcd907ab9615e2ec9dc5fac127b
William B <william.buwembo21@gmail.com>
2024-11-19 13:52:30 +0100

    Variabeltilordning

--- emne-3/Uke1-2/Datatyper/Datatyper/Program.cs
+++ emne-3/Uke1-2/Datatyper/Datatyper/Program.cs
@@ -16,24 +16,3 @@ char text2 = 'B';
 int a = 5;
 decimal b = 3;
 decimal sum = a + b;
-
-
-
-// --- Variabeltilordning --- 
-// Oppgave 1
-int num1 = 1;
-int num2 = 2;
-
-static int Tall(int tall1, int tall2)
-{
-    return tall1 + tall2;
-}
-Console.WriteLine($"Oppgave 1: {Tall(num1, num2)}");
-
-// Oppgave 2
-static void Setning()
-{
-    Console.WriteLine("Oppgave 2: Denne metoden returnerer ingenting");
-}
-Setning();
-
---
+++ emne-3/Uke1-2/Variabeltilordning/Variabeltilordning/Program.cs
@@ -0,0 +1,18 @@
+﻿// --- Variabeltilordning --- 
+// Oppgave 1
+int num1 = 1;
+int num2 = 2;
+
+static int Tall(int tall1, int tall2)
+{
+    return tall1 + tall2;
+}
+Console.WriteLine($"Oppgave 1: {Tall(num1, num2)}");
+
+// Oppgave 2
+static void Setning()
+{
+    Console.WriteLine("Oppgave 2: Denne metoden returnerer ingenting");
+}
+Setning();
+
