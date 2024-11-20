e310c55a5fbe591781b70b5ea00a47642e308c5a
William B <william.buwembo21@gmail.com>
2024-11-20 10:31:15 +0100

    random

---
+++ emne-3/Uke1-2/Random Hobby generator/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Type your name: ");
+string name = Console.ReadLine();
+
+Console.WriteLine($"Hi my name is {name} and im a ");
+RandomHobby();
+
+void RandomHobby()
+{
+    Random random = new Random();
+    int number = random.Next(0, 5);
+
+    switch (number)
+    {
+        case 0:
+            Console.WriteLine("0");
+            break;
+        case 1:
+            Console.WriteLine("1");
+            break;
+        case 2:
+            Console.WriteLine("2");
+            break;
+        case 3:
+            Console.WriteLine("3");
+            break;
+        case 4:
+            Console.WriteLine("4");
+            break;
+        case 5:
+            Console.WriteLine("5");
+            break;
+    }
+
+}
--- emne-3/Uke1-2/Random Hobby generator/Random Hobby generator/Program.cs
+++
@@ -1,2 +0,0 @@
-﻿// Oppgave:
-// Skriv et navn i konsollen, la en funksjon trekke en tilfeldig hobby og vis dette sammen med navnet i konsollvunduet.
