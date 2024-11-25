98265a121c3ca3207a19c152887074abf4593521
William B <william.buwembo21@gmail.com>
2024-11-25 08:30:56 +0100

    uke3

---
+++ emne-3/Uke3/Krokodillespillet/Program.cs
@@ -0,0 +1,35 @@
+﻿int points = 0;
+bool isRunning = true;
+
+while(isRunning)
+{
+    var rand = new Random();
+    int number1 = rand.Next(0, 10);
+    int number2 = rand.Next(0, 10);
+
+    Console.WriteLine($"{number1}_{number2}");
+    var input = Console.ReadLine();
+
+    switch (input)
+    {
+        case "<":
+            if (number1 <= number2) points++;
+            else points--;
+            break;
+        case ">":
+            if (number1 >= number2) points++;
+            else points--;
+            break;
+        default:
+            isRunning = false;
+            break;
+    }
+
+    Console.WriteLine($"points: {points}\n");
+}
+
+
+
+
+
+
