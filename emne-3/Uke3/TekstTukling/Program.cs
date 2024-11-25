4dd8ad359e0894f54d35480671ffd49232866e41
William B <william.buwembo21@gmail.com>
2024-11-25 13:04:02 +0100

    TekstTukling

---
+++ emne-3/Uke3/TekstTukling/Program.cs
@@ -0,0 +1,34 @@
+﻿string newString = "";
+
+Console.Write("1. RotateText\n2. Change word\n\nSelect: ");
+var select = Console.ReadLine();
+
+switch(select)
+{
+    case "1":
+        rotateText();
+        Console.WriteLine($"Input rotated:\n{newString}");
+        break;
+    case "2": 
+        ChangeText();
+        Console.WriteLine($"{newString}");
+        break;
+}
+
+void rotateText()
+{
+    var input = Console.ReadLine();
+    string newInput = "";
+    for (int i = input.Length - 1; i >= 0; i--)
+    {
+        newInput += input[i];
+    }
+    newString = newInput;
+}
+
+void ChangeText()
+{
+    string input = Console.ReadLine();
+    string newInput = input.Replace("c", "e");
+    newString = newInput;   
+}
