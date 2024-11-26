5d1c96844479749718533f330c15dd006ea5be3e
William B <william.buwembo21@gmail.com>
2024-11-26 18:45:08 +0100

    uke 3

---
+++ emne-3/Uke3/CountCharacters/Program.cs
@@ -0,0 +1,19 @@
+﻿var range = 250;
+var counts = new int[range];
+string text = "something";
+while (!string.IsNullOrWhiteSpace(text))
+{
+    text = Console.ReadLine();
+    foreach (var character in text ?? string.Empty)
+    {
+        counts[(int)character]++;
+    }
+    for (var i = 0; i < range; i++)
+    {
+        if (counts[i] > 0)
+        {
+            var character = (char)i;
+            Console.WriteLine(character + " - " + counts[i]);
+        }
+    }
+}
