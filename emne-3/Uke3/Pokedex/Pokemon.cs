2236a014de5e116ec804e0a508005bb917488043
William B <william.buwembo21@gmail.com>
2024-11-26 18:45:50 +0100

    Pokedex

---
+++ emne-3/Uke3/Pokedex/Pokemon.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokedex
+{
+    internal class Pokemon
+    {
+        public string name;
+        public int lvl;
+        public int hp;
+
+        public Pokemon(string Name, int Lvl, int Hp)
+        {
+            name = Name;
+            lvl = Lvl;
+            hp = Hp;
+        }
+
+        public void Run()
+        {
+            Console.WriteLine($"{name} has {lvl}hp and is {hp}lvl");
+        }
+    }
+}
---
+++ emne-3/Uke3/Pokedex/Program.cs
@@ -0,0 +1,12 @@
+﻿using Pokedex;
+
+var pikachu = new Pokemon("Pikachu", 21, 40);
+var bulbasaur = new Pokemon("Bulbasaur", 10, 20);
+var squirtle = new Pokemon("Squirtle", 35, 50);
+var charzard = new Pokemon("Charzard", 99, 100);
+
+pikachu.Run();
+bulbasaur.Run();
+squirtle.Run();
+charzard.Run();
+
