52a48ca91910437692d1bc5f73ccaa4999a6c3a6
William B <william.buwembo21@gmail.com>
2024-11-29 14:44:11 +0100

    Overloads

---
+++ emne-3/Uke3/Overloads/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Overloads
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WelcomeMessage w = new WelcomeMessage();
+            
+            Console.WriteLine($"{w.PrintWelcomeMessage()}, {w.Compliment()}"); // Default value
+            Console.WriteLine($"{w.PrintWelcomeMessage()}, {w.Compliment("du er kul")}"); // Overloaded default value
+        }   
+    }
+
+    class WelcomeMessage
+    {
+        public string PrintWelcomeMessage() 
+        {
+            return "Hei og velkommen";
+        }
+
+        public string Compliment(string compliment = "du er snill") // Method with a default value
+        {
+            return compliment;
+        }
+    }
+}
