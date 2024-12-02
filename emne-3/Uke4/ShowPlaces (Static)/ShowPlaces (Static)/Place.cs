72811579253b403378f9270670de04769713edb2
William B <william.buwembo21@gmail.com>
2024-12-02 14:20:55 +0100

    Objekter via list

---
+++ emne-3/Uke4/ShowPlaces (Static)/ShowPlaces (Static)/Place.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShowPlaces__Static_
+{
+    internal class Place
+    {
+        public static void ShowPlace(string place, string municipality, string region)
+        {
+            int labelWidth = 8;
+            ShowSeparatorRow();
+            ShowFieldNameAndValue("Navn", labelWidth, place);
+            ShowFieldNameAndValue("Komune", labelWidth, municipality);
+            ShowFieldNameAndValue("Fylke", labelWidth, region);
+            ShowSeparatorRow();
+        }
+
+        static void ShowFieldNameAndValue(string label, int labelWidth, string fieldValue)
+        {
+            labelWidth -= label.Length;
+            Console.WriteLine($"  {label}:{String.Empty.PadLeft(labelWidth, ' ')}{fieldValue}");
+        }
+
+        static void ShowSeparatorRow()
+        {
+            Console.WriteLine(String.Empty.PadRight(20, '*'));
+        }
+    }
+}
---
+++ emne-3/Uke4/ShowPlaces (Static)/ShowPlaces (Static)/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ShowPlaces__Static_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Place.ShowPlace("Stavern", "Larvik", "Vestfold");
+            Place.ShowPlace("Alta", "Alta", "Finnmark");
+            Place.ShowPlace("Tromsø", "Tromsø", "Troms");
+        }
+    }
+}
