12549a60adfb635dcbed80ab9d457449f52de418
William B <william.buwembo21@gmail.com>
2024-12-02 15:35:51 +0100

    ShowPlaces med Objekter

---
+++ emne-3/Uke4/ShowPlaces (OOP)/ShowPlaces (OOP)/Place.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShowPlaces__OOP_
+{
+    internal class Place
+    {
+        public string PlaceName;
+        public string Municipality;
+        public string Region;
+
+        public Place(string placeName, string municipality, string region)
+        {
+            PlaceName = placeName;
+            Municipality = municipality;
+            Region = region;
+        }
+
+        public void ShowPlace()
+        {
+            int labelWidth = 8;
+            ShowSeparatorRow();
+            ShowFieldNameAndValue("Navn", labelWidth, PlaceName);
+            ShowFieldNameAndValue("Komune", labelWidth, Municipality);
+            ShowFieldNameAndValue("Fylke", labelWidth, Region);
+            ShowSeparatorRow();
+        }
+
+        void ShowFieldNameAndValue(string label, int labelWidth, string fieldValue)
+        {
+            labelWidth -= label.Length;
+            Console.WriteLine($"  {label}:{String.Empty.PadLeft(labelWidth, ' ')}{fieldValue}");
+        }
+
+        void ShowSeparatorRow()
+        {
+            Console.WriteLine(String.Empty.PadRight(20, '*'));
+        }
+    }
+}
---
+++ emne-3/Uke4/ShowPlaces (OOP)/ShowPlaces (OOP)/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ShowPlaces__OOP_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Place place1 = new Place("Stavern", "Larvik", "Vestfold");
+            Place place2 = new Place("Alta", "Alta", "Finnmark");
+            Place place3 = new Place("Tromsø", "Tromsø", "Troms");
+
+            place1.ShowPlace();
+            place2.ShowPlace();
+            place3.ShowPlace();
+        }
+    }
+}
