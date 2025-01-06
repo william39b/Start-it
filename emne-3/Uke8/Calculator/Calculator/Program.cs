8c4bec466f6b043578aadaeb1cb728459ab25e21
William B <william.buwembo21@gmail.com>
2025-01-06 11:44:35 +0100

    ...

--- emne-3/Uke8/Calculator/Calculator/Program.cs
+++
@@ -1,3 +0,0 @@
-﻿// Oppgave: Unittesting for en Kalkulator
-// Du har blitt gitt en enkel kalkulatorklasse, og oppgaven din er å skrive unittests for denne klassen.
-Console.WriteLine("Hello, World!");
