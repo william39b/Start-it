8f403b1b81b124feb9ff6417b925a9a28d7fab3e
William B <william.buwembo21@gmail.com>
2025-01-16 07:28:16 +0100

    CodeAlong-02

---
+++ emne-3/CodeAlong/CodeAlong-02/CodeAlong-02/Car.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeAlong_02
+{
+    internal class Car
+    {
+        public int Id { get; private set; }
+        public int Speed { get; private set; }
+        public int StartSpeed { get; private set; }
+        public int TopSpeed { get; private set; }
+        public int Distance { get; private set; }
+
+        public Car(int id, int startSpeed)
+        {
+            Id = id;
+            Speed = 0;
+            StartSpeed = startSpeed;
+            TopSpeed = 0;
+            Distance = 0;
+        }
+
+        public void RandomSpeed()
+        {
+            var random = new Random();
+            var newSpeed = random.Next(60, 200);
+            TopSpeed = newSpeed;
+        }
+
+        public void StartRace()
+        {
+            int distance = 10000;
+            int sec = 0;
+
+            while (true)
+            {
+                sec++;
+                if (Distance < (distance / 2))
+                {
+                    SpeedUp();
+                }
+                else
+                {
+                    SlowDown();
+                }
+                //Console.WriteLine($"Car{Id} speed: {Speed} distance: {Distance}");
+
+                if (Distance >= distance)
+                {
+                    Console.WriteLine($"Car{Id} finished the race in {sec} seconds");
+                    break;
+                }
+            }
+
+        }
+
+        private void SpeedUp()
+        {
+            if (Speed <= TopSpeed)
+            {
+                Speed += StartSpeed;
+            }
+            Distance += Speed;
+        }
+
+        private void SlowDown()
+        {
+            if (Speed > StartSpeed)
+            {
+                Speed -= StartSpeed;
+            }
+            Distance += Speed;
+        }
+
+    }
+}
+
---
+++ emne-3/CodeAlong/CodeAlong-02/CodeAlong-02/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Du skal lage en konsollapp med en bil som skal kjøre 1000m. 
+Bilen har en start-hastighet på 10m/s (en iterasjon i en løkke er et sekund).
+Bilen skal øke farten med 10m/s frem til den har kjørt 500m, 
+også skal den senke farten med 10m/s 
+igjen frem til den når minimumshastigheten sin på 10m/s.
+Når bilen har kommet seg til slutt-målet printes det ut til skjermen at bilen er fremme og har parkert.
+
+Utvid oppgaven til at du kan lage 2 biler som kjører om kapp i en løkke, 
+der de har en metode som genererer en random hastighet til dem mellom 60-200m/s som varer en runde. 
+Det er førstemann til å kjøre 10000m
+*/
+
+using CodeAlong_02;
+
+List<Car> drivers = [
+    new Car(1, 10),
+    new Car(2, 10),
+];
+
+foreach (Car car in drivers)
+{
+    new RaceTrack(car);
+}
